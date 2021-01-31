e70f9e3562dde0bb5988d3920061cd32299affb0
Xazmah <40501994+Xazmah@users.noreply.github.com>
2021-01-31 19:08:30 +0100

    Fix matchRegex for Snahp
    Tested and working

--- IMDb_Scout_Mod.user.js
+++ IMDb_Scout_Mod.user.js
@@ -1703,7 +1703,7 @@ var private_sites = [
   {   'name': 'Snahp',
       'searchUrl': 'https://forum.snahp.it/search.php?keywords=%tt%',
       'loggedOutRegex': /register" role/,
-      'matchRegex': '/Search found 0 matches/',
+      'matchRegex': /Search found 0 matches|No suitable matches were found/,
       'both': true},
   {   'name': 'SP',
       'searchUrl': 'https://www.scenepalace.info/browse.php?search=%nott%&cat=0&incldead=1',
